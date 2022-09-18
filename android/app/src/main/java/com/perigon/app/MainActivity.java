4191a5153a6000bbad6267108307f48219a9bad0
VCSNDBX <vbcatapat@gmail.com>
2022-09-18 13:23:44 +0800

    added iOS and Android build

---
+++ android/app/src/main/java/com/perigon/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.perigon.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.perigon.app',
   appName: 'Perigon',
   webDir: 'dist',
   bundledWebRuntime: false
