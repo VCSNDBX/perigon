86fa1b6e309fbacd54f8ef228fdb8d486587c6c9
VCSNDBX <vbcatapat@gmail.com>
2022-09-21 13:25:54 +0800

    landing

---
+++ src/components/LandingSlider.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="flex flex-col">
+        <div class="bg-black rounded-full mx-auto mb-8 p-12">
+            <img :src="img">
+        </div>
+        <div class="text-center text-black text-xl font-bold">
+        {{ title }}
+        </div>
+        <div class="text-center text-gray-500 text-xl font-normal px-8">
+        {{ description }}
+        </div>
+        <div class="flex flex-col justify-center pt-20">
+            <ion-button class="bg-black rounded-md mx-auto px-16 text-white font-bold" @click="next">NEXT</ion-button>
+            <ion-button  fill="clear" color="secondary" class="text-black font-bold px-14 py-3 mx-auto text-sm" @click="skip">SKIP</ion-button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { useIonRouter } from '@ionic/vue';
+import { useSwiper } from 'swiper/vue';
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "LandingSlider",
+    
+    props: {
+        position: Number,
+        img: String,
+        title: String,
+        description: String,
+    },
+    
+    setup() {
+        const ionRouter = useIonRouter();
+        const swiper = useSwiper();
+        
+        return {
+            swiper,
+            ionRouter,
+        };
+    },
+
+    methods: {
+         next(){
+            if(this.swiper.isEnd == true){
+              return this.ionRouter.navigate("/login/", 'forward', 'replace');
+            }
+            else{
+              this.swiper.slideNext()
+           }
+        },
+
+        skip() {
+            this.ionRouter.navigate("/login/", 'forward', 'replace');
+        },
+  },
+})
+</script>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,102 +1,78 @@
 <template>
     <ion-page>
         <ion-content :fullscreen="true" class="flex justify-center flex-col w-screen">
-            <ion-slides :pager="true" :options="slideOpts" ref="mySlides" class="mt-20">
-              <ion-slide class="flex flex-col">
-                  <div class="bg-black rounded-full mx-auto mb-8 p-12">
-                      <img :src="ticket">
-                  </div>
-                  <div class="text-center text-black text-xl font-bold">
-                    BUY PASSES
-                  </div>
-                  <div class="text-center text-gray-500 text-xl font-normal px-8">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar ac neque, enim cursus est facilisis.
-                  </div>
-              </ion-slide>
-              <ion-slide class="flex flex-col">
-                  <div class="bg-black rounded-full mx-auto mb-10 p-12">
-                    <img :src="ride">
-                  </div>
-                  <div class="text-center text-black text-xl font-bold py-2">
-                    BOOK A RIDE
-                  </div>
-                  <div class="text-center text-gray-500 text-xl font-normal py-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar ac neque, enim cursus est facilisis.
-                  </div>
-              </ion-slide>
-              <ion-slide class="flex flex-col">
-                  <div class="bg-black rounded-full mx-auto mb-10 p-12">
-                    <img :src="pay">
-                  </div>
-                  <div class="text-center text-black text-xl font-bold py-2">
-                    PAY AT YOUR CONVINENCE
-                  </div>
-                  <div class="text-center text-gray-500 text-xl font-normal py-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar ac neque, enim cursus est facilisis.
-                  </div>
-              </ion-slide>
-          </ion-slides>
-          <div class="flex flex-col justify-center">
-            <ion-button fill="clear" class="bg-black rounded-md mx-auto px-16 text-white font-bold" @click="nextSlide">NEXT</ion-button>
-            <ion-button  fill="clear" color="secondary" class="text-black font-bold px-14 py-3 mx-auto text-sm" @click="skip">SKIP</ion-button>
-          </div>
+            <swiper 
+            :slides-per-view="1"
+            :modules="modules"
+            @slideChange="onSlideChange"
+            class="pt-44"
+            >
+              <swiper-slide
+                v-for="(slides, index) in slideContent"
+                :key="index"
+                :virtualIndex="index"
+                class=""
+              >
+              <LandingSlider
+                :position="index"
+                :img="slides.img"
+                :title="slides.title"
+                :description="slides.description"
+              ></LandingSlider>
+            </swiper-slide>
+          </swiper>
         </ion-content>
     </ion-page>
 </template>
  
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import { IonSlides, IonSlide, IonPage, IonContent, useIonRouter } from '@ionic/vue'
+import { defineComponent, ref} from 'vue'
+import { IonPage, IonContent, } from '@ionic/vue'
+import { Navigation } from 'swiper';
+import { Swiper, SwiperSlide, } from 'swiper/vue';
+import LandingSlider from '@/components/LandingSlider.vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 export default defineComponent({
     components: {
         IonPage,
-        IonSlides,
-        IonSlide,
         IonContent,
+        Swiper,
+        SwiperSlide,
+        LandingSlider,
     },
-
-    data() {
-        return {
-            ticket: "/assets/img/ticket.png",
-            ride: "/assets/img/ride.png",
-            pay: "/assets/img/pay.png",
-        };
-    },
-
+    
     setup() {
-        const ionRouter = useIonRouter();
-
-        const mySlides = ref<any>(null);
-
-        const slideOpts = {
-            initialSlide: 0,
-            speed: 400,
-        };
-
-        const nextSlide = async () => {
-          const s = await mySlides?.value?.$el.getSwiper();
-          await s.slideNext();
+        const onSlideChange = async () => {
+            console.log("changed slider");
         };
 
         return { 
-            slideOpts,
-            ionRouter,
-            mySlides,
-            nextSlide,
+            onSlideChange,
+            modules: [ Navigation ],
+            slideContent: [
+              {
+                img: "/assets/img/ticket.png",
+                title: "BUY PASSES",
+                description:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar  ac neque, enim cursus est fascilisis.",
+              },
+              {
+                img: "/assets/img/ride.png",
+                title: "BOOK A RIDE",
+                description:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar  ac neque, enim cursus est fascilisis.",
+              },
+              {
+                img: "/assets/img/pay.png",
+                title: "PAY AT YOUR CONVINENCE",
+                description:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pulvinar  ac neque, enim cursus est fascilisis.",
+              },
+            ],
         }
     },
-
-    methods: {
-        skip() {
-            this.ionRouter.navigate("/login/", 'forward', 'replace');
-        },
-  },
 })
  </script>
-
-<style scoped>
-.swiper-slide {
-  height: 450px;
-}
-</style>
